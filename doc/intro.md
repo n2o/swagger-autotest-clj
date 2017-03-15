eb025c533bfed3e88739fb26471492d7fc206897
Christian Meter <christian.meter@uni-duesseldorf.de>
2017-03-15 07:55:26 +0100

    Prepare clojure project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to swagger-autotest
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ project.clj
@@ -0,0 +1,11 @@
+(defproject swagger-autotest "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.9.0-alpha15"]
+                 [io.forward/yaml "1.0.6"]]
+  :plugins [[lein-ancient "0.6.10"]]
+  :main ^:skip-aot swagger-autotest.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
