82353637c24e1f02a71dae696d650d91f815998d
Christian Meter <christian.meter@uni-duesseldorf.de>
2017-03-24 17:36:24 +0100

    Prepare tests; add sample yaml configuration

--- test/swagger_autotest/core_test.clj
+++ test/swagger_autotest/core_test.clj
@@ -1,7 +1,20 @@
 (ns swagger-autotest.core-test
   (:require [clojure.test :refer :all]
+            [clojure.spec :as s]
             [swagger-autotest.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def test-config
+  (load-config "swagger.yml"))
+
+
+;; -----------------------------------------------------------------------------
+;; Tests
+
+(deftest test-base-url
+  (testing "Get base-route from configuration"
+    (let [url (construct-base-url test-config)]
+      (is (= "http://localhost:4284/api" url)))))
+
+(deftest test-prepare-query
+  (testing "Given an entry form the configuration, extract and reformat all information"
+    ))
