d9f4f85c2a70cce5437af1a76859a733f934aa76
Christian Meter <christian.meter@uni-duesseldorf.de>
2017-03-24 17:35:53 +0100

    Update dependencies

--- project.clj
+++ project.clj
@@ -4,8 +4,10 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha15"]
-                 [io.forward/yaml "1.0.6"]]
-  :plugins [[lein-ancient "0.6.10"]]
+                 [io.forward/yaml "1.0.6"]
+                 [clj-http "3.4.1"]]
+  :plugins [[lein-ancient "0.6.10"]
+            [com.jakemccrary/lein-test-refresh "0.19.0"]]
   :main ^:skip-aot swagger-autotest.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
