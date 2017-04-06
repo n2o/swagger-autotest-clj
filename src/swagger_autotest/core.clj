f59420b848bef7cdf5aa9a11a9617b74b9133f16
Christian Meter <christian.meter@uni-duesseldorf.de>
2017-04-06 17:17:45 +0200

    Move parsing fns to separate namespace

--- src/swagger_autotest/core.clj
+++ src/swagger_autotest/core.clj
@@ -1,32 +1,6 @@
 (ns swagger-autotest.core
-  (:refer-clojure :exclude [load])
-  (:require [clojure.spec :as s]
-            [yaml.core :as yaml]
-            [clojure.walk :refer [keywordize-keys]]
-            [clj-http.client :as client])
+  (:require [clj-http.client :as client])
   (:gen-class))
-
-(defn load-config [path]
-  (keywordize-keys (yaml/from-file path)))
-
-(defn construct-base-url [config]
-  (str (first (:schemes config)) "://"
-       (:host config)
-       (:basePath config)))
-
-(def config
-  (load-config "swagger.yml"))
-
-(def base
-  (construct-base-url config))
-;; (base y)
-
-(second (first (:paths config)))
-
-(s/def ::protocol #{:get :post :put :delete})
-
-
-
 
 (defn prepare-query [entry]
   (let [route (str (first entry))
@@ -34,8 +8,6 @@
         definitions (second entry)]
     [uri definitions]))
 (prepare-query (first (:paths config)))
-
-
 
 
 
--- src/swagger_autotest/specs.clj
+++ src/swagger_autotest/specs.clj
@@ -1,4 +1,19 @@
-(ns swagger-autotest.specs)
+(ns swagger-autotest.specs
+  (:require [clojure.spec :as s]
+            [clojure.spec.test :as stest]))
 
 (def url-pattern #"(?i)^(?:(?:https?)://)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?$")
 (s/def ::url (s/and string? #(re-matches url-pattern %)))
+
+(s/fdef swagger-autotest.core/construct-base-url
+        :args (s/cat :config string?))
+;; (stest/instrument 'swagger-autotest.core/construct-base-url)
+
+#_(s/fdef discuss.utils.common/trim-and-normalize
+        :args (s/cat :str string?)
+        :ret string?
+        :fn #(<= (-> % :ret count) (-> % :args :str count)))
+
+
+;; (s/exercise-fn `discuss.utils.common/trim-and-normalize)
+;; (stest/abbrev-result (first (stest/check `discuss.utils.common/trim-and-normalize)))
