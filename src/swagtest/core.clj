8b97814defce984e783878867a2980f4db527d2f
Christian Meter <christian.meter@uni-duesseldorf.de>
2017-04-07 16:13:04 +0200

    Refactor namespaces

--- src/swagtest/core.clj
+++ src/swagtest/core.clj
@@ -1,19 +1,6 @@
 (ns swagtest.core
-  (:require [clj-http.client :as client]
-            [swagtest.parse :as parse])
+  (:require [clj-http.client :as client])
   (:gen-class))
-
-(def config
-  (parse/load-config "swagger.yml"))
-
-(defn prepare-query [config]
-  (let [entry (first (:paths config))
-        base (parse/construct-base-url config)
-        route (str (first entry))
-        uri (str base (subs route 1 (count route)))
-        definitions (second entry)]
-    [uri definitions]))
-(prepare-query config)
 
 (defn -main
   "I don't do a whole lot ... yet."
---
+++ src/swagtest/parser/yaml.clj
@@ -0,0 +1,29 @@
+(ns swagtest.parser.yaml
+  (:require [yaml.core :as yaml]
+            [clojure.walk :refer [keywordize-keys]]))
+
+(defn load-config [path]
+  (keywordize-keys (yaml/from-file path)))
+
+(def config
+  (load-config "swagger.yml"))
+
+(defn construct-base-url
+  "Extract path to API from config file."
+  [config]
+  (when-not (nil? (:host config))
+    (str (first (:schemes config)) "://"
+         (:host config)
+         (:basePath config))))
+
+(defn prepare-query [config]
+  (let [entry (first (:paths config))
+        base (construct-base-url config)
+        route (str (first entry))
+        uri (str base (subs route 1 (count route)))
+        definitions (second entry)]
+    [uri definitions]))
+(prepare-query config)
+
+;; (base y)
+;; (second (first (:paths config)))
