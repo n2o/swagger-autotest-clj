a24fcf6c89013d094ae21cf47b3dfd79015ca274
Christian Meter <christian.meter@uni-duesseldorf.de>
2017-04-28 12:32:34 +0200

    Update to clj alpha 16

--- project.clj
+++ project.clj
@@ -3,10 +3,11 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha15"]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha16"]
                  [org.clojure/test.check "0.9.0"]
                  [io.forward/yaml "1.0.6"]
-                 [clj-http "3.4.1"]]
+                 [clj-http "3.4.1"]
+                 [com.velisco/strgen "0.1.4"]]
   :plugins [[lein-ancient "0.6.10"]
             [lein-kibit "0.1.3"]
             [lein-set-version "0.4.1"]]
